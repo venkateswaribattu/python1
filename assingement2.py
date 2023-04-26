95ce6b25207035c9106e9c373ce3fcb5c74a92b7
venkateswaribattu <venkateswaribattu144@gmail.com>
2023-04-26 09:49:26 +0530

    python4 file

---
+++ assingement2.py
@@ -0,0 +1,144 @@
+#1.write a program to generate all 5 divisable from 10 to 50?
+'''for i in range(10,51,5):
+    print(i)
+    '''
+#2.write a programe to generate aii 10 divisables from 100 to 10?
+'''for i in range(100,9,-10):
+    print(i)
+'''
+#3.write a programe to generate all odd numbers from 11 to 25?
+'''for i in range(11,26,2):
+    print(i)
+'''
+#4.write a programe to generate all even numbers from -2 to -22?
+'''
+for i in range(-2,-23,-2):
+    print(i)
+'''
+#5.write a programe to generate all numbers from -3 to 5?
+'''
+for i in range(-3,6,1):
+    print(i)
+'''
+#6.write a programe to generate all 7 divisibles from -21 to 21?
+'''
+for i in range(-21,22,7):
+    print(i)
+'''
+#7.write a programe to generate all numbers from 1 to 10 except 2 and 6?
+'''
+i=1
+while i<11:
+    if i==2:
+        i=i+1
+        continue
+    if i==6:
+        i=i+1
+        continue
+    print(i)
+    i=i+1
+ '''
+#8.write a programe to generate all numbers from 15 to 1 except 3,6,9and12?
+'''
+i=15
+while i>1:
+    if i==3 or 6 or 9 or 12:
+        i=i-1
+        continue
+    print(i)
+    i=i+1
+  '''
+#9.write a programe to generate all numbers from -2 to 2  except -1 and 1?
+'''
+i=-2
+while i<3:
+    if i==-1:
+        i=i+1
+        continue
+    if i==1:
+        i=i+1
+        continue
+    print(i)
+    i=i+1
+    '''
+#1.write a programe to generate aii 5 divisable from 10 to 50?
+'''
+i=10
+while i<51:
+        print(i)
+        i=i+5
+'''
+#2.write a programe to generate all 10 divisable from 100 to 10?
+'''
+i=100
+while i>9:
+        print(i)
+        i=i-10
+        '''
+#3.write a programe to generate all odd numbers from 11 to 25?
+'''
+i=11
+while i<25:
+        i=i+2
+        print(i)
+        '''
+#4.write a programe to generate all even numbers from -2 to -22?
+'''
+i=-2
+while i>-22:
+        i=i-2
+        print(i)
+        '''
+#5.write a programe to generate all numbers from -3 to 5?
+'''
+i=-3
+while i<6:
+        print(i)
+        i=i+1
+        '''
+#6.write a programe to generate all 7 divisables from -21 to 21?
+'''
+for i in range(-21,22,7):
+        print(i)
+        '''
+#7.write a programe to generate all numbers from 1 to 10 exceot 2and 6?
+'''
+for i in range(1,11,1):
+        if i==2 or i==6:
+                continue
+        print(i)
+        '''
+#8.write a programe to generate all numbers from 15 to 1 except 3,6,9 and 12?
+'''
+for i in range(15,0,-1):
+        if i==3 or i==6 or i==9 or i==12:
+                continue
+        print(i)
+        '''
+#9.write a programe to generate all numbers from -2 to 2 except -1 and 1?
+'''
+for i in range(-2,3,1):
+        if i==-1 or i==1:
+                continue
+        print(i)
+   '''
+#10.write a programe to generate all 3 divisables from 15 to-15 except even numbers?
+i=15
+while i>1:
+        if i==3 or i==6 or i==9 or i==12:
+                continue
+        print(i)
+        
+
+
+
+
+                
+                
+
+
+                
+                
+        
+        
+        
